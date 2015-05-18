9ad4b9fb0ff95ad89f8782249c37de97a529d0a8
Ryan Swaney <ryan.swaney@gmail.com>
2015-05-18 16:32:11 -0400

    Add Social Share links post-launch

--- header.php
+++ header.php
@@ -24,7 +24,33 @@
 <div id="content" class="site-content">
 
 <header class="centered-navigation" role="banner">
+
+  <div class="social-share">
+    <ul>
+      <li>
+        <span class="share-button-label">Share on:</span>
+      </li>
+      <li>
+        <a class="js-social-share" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode( home_url( '/' ) ); ?>" target="_blank">
+        <span class="share-button-icon">
+          <svg viewBox="0 0 33 33" preserveAspectRatio="xMidYMid meet" class="svg-icon svg-icon-facebook"><path d="M 17.996,32L 12,32 L 12,16 l-4,0 l0-5.514 l 4-0.002l-0.006-3.248C 11.993,2.737, 13.213,0, 18.512,0l 4.412,0 l0,5.515 l-2.757,0 c-2.063,0-2.163,0.77-2.163,2.209l-0.008,2.76l 4.959,0 l-0.585,5.514L 18,16L 17.996,32z"></path></svg>
+        </span>
+        <span class="share-button-label">Facebook</span>
+      </a>
+      </li>
+      <li>
+        <a class="js-social-share" href="https://twitter.com/intent/tweet/?text=Check%20out%20the%20World%20Happiness%20Report%202015!&url=<?php echo urlencode( home_url( '/' ) ); ?>&via=UNSDSN" target="_blank">
+          <span class="share-button-icon">
+            <svg viewBox="0 0 12 12" preserveAspectRatio="xMidYMid meet" class="svg-icon svg-icon-twitter"><path class="svg-icon-path" d="M10.8,3.5c0,0.1,0,0.2,0,0.3c0,3.3-2.5,7-7,7c-1.4,0-2.7-0.3-3.8-1c0.2,0,0.4,0,0.6,0c1.1,0,2.2-0.5,3.1-1.2 c-1.1,0-2-0.7-2.3-1.7c0.2,0,0.3,0,0.5,0s0.4,0,0.7-0.1c-1.1-0.2-2-1.2-2-2.4l0,0c0.3,0.2,0.7,0.3,1.1,0.3C1,4.3,0.6,3.5,0.6,2.7 c0-0.5,0.1-0.8,0.3-1.2c1.2,1.6,3,2.5,5,2.6c0-0.2-0.1-0.4-0.1-0.6c0-1.3,1.1-2.4,2.5-2.4c0.7,0,1.4,0.3,1.8,0.8 c0.6-0.1,1.1-0.3,1.6-0.6c-0.2,0.6-0.6,1.1-1.1,1.4c0.5-0.1,1-0.2,1.4-0.4C11.7,2.8,11.2,3.2,10.8,3.5z"/></svg>
+          </span>
+          <span class="share-button-label">Twitter</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+
   <div class="centered-navigation-wrapper">
+
     <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="mobile-logo">
       <img src="<?php the_field('logo_mobile', 'option'); ?>">
     </a>
--- js/happiness.js
+++ js/happiness.js
@@ -17,16 +17,6 @@ jQuery(document).ready(function( $ ) {
   (function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[4]='MMERGE4';ftypes[4]='text';fnames[5]='MMERGE5';ftypes[5]='text';fnames[3]='MMERGE3';ftypes[3]='phone';}(jQuery));var $mcj = jQuery.noConflict(true);
 
 
-  // Google Analytics
-   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-34440011-4', 'auto');
-  ga('send', 'pageview');
-
-
   // TypeKit Font loader
   WebFontConfig = {
       typekit: { id: 'vaz6tdr' }
@@ -37,6 +27,38 @@ jQuery(document).ready(function( $ ) {
       wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.5.6/webfont.js';
       s.parentNode.insertBefore(wf, s);
    })(document);
+
+   // Social Share Popup
+   function windowPopup(url, width, height) {
+  // Calculate the position of the popup so
+  // it’s centered on the screen.
+  var left = (screen.width / 2) - (width / 2),
+      top = (screen.height / 2) - (height / 2);
+
+  window.open(
+    url,
+    "",
+    "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=" + width + ",height=" + height + ",top=" + top + ",left=" + left
+  );
+}
+
+$(".js-social-share").on("click", function(e) {
+  e.preventDefault();
+
+  windowPopup($(this).attr("href"), 500, 300);
+});
+
+// Vanilla JavaScript
+var jsSocialShares = document.querySelectorAll(".js-social-share");
+if (jsSocialShares) {
+  [].forEach.call(jsSocialShares, function(anchor) {
+    anchor.addEventListener("click", function(e) {
+      e.preventDefault();
+
+      windowPopup(this.href, 500, 300);
+    });
+  });
+}
 
 
 });
